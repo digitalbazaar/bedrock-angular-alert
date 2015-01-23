865d030591f0dc5c22535151c495daac6b9be7cb
Dave Longley <dlongley@digitalbazaar.com>
2015-01-23 18:24:35 -0500

    Update copyright (2015).

--- alerts-directive.js
+++ alerts-directive.js
@@ -1,7 +1,7 @@
 /*!
  * Alerts directive.
  *
- * Copyright (c) 2014 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2014-2015 Digital Bazaar, Inc. All rights reserved.
  *
  * @author Dave Longley
  */
